078e06422029f5bb00979f4db9fff23de1954cc8
PawelKowalczuk00 <german4050@wp.pl>
2020-02-02 17:24:06 +0100

    fixed verificarion links

--- functions/createVeryficationLink.js
+++ functions/createVeryficationLink.js
@@ -3,6 +3,6 @@ import jwt from 'jsonwebtoken';
 export default (id) => {
     const token = jwt.sign({ id }, global.jwtKey, { expiresIn: '1d'});
     if (process.env.NODE_ENV === "production")
-        return `http://heroku-app--------/api/emailVeryfication/${token}`
+        return `https://internet-shop-project-pk2020.herokuapp.com/api/emailVeryfication/${token}`
     return `http://localhost:${global.port}/api/emailVeryfication/${token}`;
 } 
