86f66b0d1b53e105a7065904e9c448cb97892000
PawelKowalczuk00 <german4050@wp.pl>
2020-02-03 01:08:42 +0100

    refreshing build

--- client/src/Components/LoaderComponent.js
+++ client/src/Components/LoaderComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
     return (
-        <div className="d-flex justify-content-center align-items-center">
+        <div className="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-7 d-flex justify-content-center align-items-center">
             <div className="spinner-border" role="status">
                 <span className="sr-only">Loading...</span>
             </div>
