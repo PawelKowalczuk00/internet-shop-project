b6def1a530a9ff53241920aef37fef52e703d331
PawelKowalczuk00 <german4050@wp.pl>
2020-02-03 01:30:15 +0100

    google ads

--- client/src/Routes/Verified.js
+++ client/src/Routes/Verified.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Verified = () => {
-        return (
-            <div className="text-center col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-7">
-                <h2 className="text-success">You have succesfully verified your account!</h2>
-                <Link to="/account" className="btn btn-succes btn-block">Visit yor account</Link>
-            </div>
-        );
+    return (
+        <div className="text-center col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-7">
+            <h2 className="text-success">You have succesfully verified your account!</h2>
+            <Link to="/account">
+                <div className="btn btn-succes btn-block">Visit yor account</div>
+            </Link>
+        </div>
+    );
 }
 
 export default Verified;
