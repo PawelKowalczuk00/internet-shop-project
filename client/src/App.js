723f2abd7b4f95b3f2081aa7ef5b1524448949e2
PawelKowalczuk00 <german4050@wp.pl>
2020-02-19 19:40:50 +0100

    version control

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ const App = () => {
       </BrowserRouter>
       <footer>
         Paweł Kowalczuk - Internet Shop
-        <span className="text-right">V:1.2.1</span>
+        <span className="text-right">V:1.2.2</span>
       </footer>
     </Provider>
   );
