9a3665253697f748715df7e84be45dc53483f4b8
PawelKowalczuk00 <german4050@wp.pl>
2020-02-19 17:11:08 +0100

    Version update

--- client/src/Routes/Home.js
+++ client/src/Routes/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <h1>V:1.1.0</h1>
+                <h1>V:1.1.1</h1>
             </div>
         );
     }
